a18c1bfef56990d8d3e95bde17699a2cf3ca322c
fteerxpat <fluketeerapatlearning@gmail.com>
2026-03-06 13:43:39 +0700

    Add submission.json for final grading

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -37,7 +37,7 @@ export default defineConfig((ctx) => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#build
     build: {
       env: {
-        API_URL: 'https://ch3-1-proxy-server.onrender.com/api'
+        API_URL: 'https://teerapat-phojit-6604101339-06-03-2026.onrender.com/api'
       },
       // publicPath: '/',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
